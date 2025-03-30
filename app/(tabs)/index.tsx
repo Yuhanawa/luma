a250d57dd41b9763fa722693590f2f6138ff4bac
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2025-03-30 23:16:05 +0800

    feat: add title and swipe for unread TopicPanel

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
+import { Cat } from "lucide-react-native";
 import { useState } from "react";
-import { View, useWindowDimensions } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Alert, View, useWindowDimensions } from "react-native";
 import { SceneMap, TabBar, TabView } from "react-native-tab-view";
 import { useTheme } from "~/components/providers/ThemeProvider";
 import { TopicPanel } from "~/components/topic/TopicPanel";
@@ -12,7 +12,23 @@ const ROUTES = [
 
 const SCENE_MAP = SceneMap({
 	all: () => <TopicPanel listTopics="listLatestTopics" />,
-	unread: () => <TopicPanel listTopics="listUnreadTopics" />,
+	unread: () => (
+		<TopicPanel
+			title="Unread Topics"
+			listTopics="listUnreadTopics"
+			swipe={[
+				// TODO
+				{
+					text: "say nya!",
+					onPress: (item) => {
+						Alert.alert(`nya! (${item?.id})`);
+					},
+					backgroundColor: "#ffa9a3",
+					icon: <Cat size={24} className="text-sky-400" />,
+				},
+			]}
+		/>
+	),
 });
 
 const renderTabBar =
