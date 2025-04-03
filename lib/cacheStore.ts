e3f9cd3f18a7c3121f0d03d97fd9cebbe6641ba0
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2025-04-04 02:48:43 +0800

    feat: add cache key for createCacheStore item and add usePostsCache

--- lib/cacheStore.ts
+++ lib/cacheStore.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidV4 } from "uuid";
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
@@ -6,19 +7,19 @@ export interface CacheStoreItem<T> {
 	value: T;
 	expired: Date;
 }
-
+type CacheStoreItemValue<T> = T & { __CACHE_IDENTIFIER__?: string };
 export interface CacheStoreState<T> {
-	caches: Map<string, CacheStoreItem<T>>;
+	caches: Map<string, CacheStoreItem<CacheStoreItemValue<T>>>;
 	lifetime: number;
-	get: (name: string) => T | null;
-	set: (name: string, value: T | null) => void;
+	get: (name: string) => CacheStoreItemValue<T> | null;
+	set: (name: string, value: CacheStoreItemValue<T> | null) => void;
 }
 
 export function createCacheStore<T>(name: string, lifetime: number = 10 * 60 * 1000) {
 	return create<CacheStoreState<T>>()(
 		devtools(
 			(set, get) => ({
-				caches: new Map<string, CacheStoreItem<T>>(),
+				caches: new Map<string, CacheStoreItem<CacheStoreItemValue<T>>>(),
 				lifetime: lifetime,
 				get: (name) => {
 					const item = get().caches.get(name);
@@ -31,10 +32,12 @@ export function createCacheStore<T>(name: string, lifetime: number = 10 * 60 * 1
 						get().caches.delete(name);
 						return;
 					}
-					const { caches, lifetime } = get();
+					const { caches, lifetime, get: getCache } = get();
+					const oldValue = getCache(name);
+					if (oldValue !== null && oldValue?.__CACHE_IDENTIFIER__ === value.__CACHE_IDENTIFIER__) return;
 					caches.set(name, {
 						name: name,
-						value: value,
+						value: { __CACHE_IDENTIFIER__: uuidV4(), ...value },
 						expired: new Date(Date.now() + lifetime),
 					});
 					set(() => ({ caches }));
--- store/cacheStore.ts
+++ store/cacheStore.ts
@@ -1,4 +1,6 @@
 import type { TopicCardItem } from "~/components/topic/TopicCard";
 import { createCacheStore } from "~/lib/cacheStore";
+import type { GetTopic200 } from "~/lib/gen/api/discourseAPI/schemas";
 
 export const useTopicsCache = createCacheStore<TopicCardItem[]>("topics");
+export const usePostsCache = createCacheStore<GetTopic200>("posts");
