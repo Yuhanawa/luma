5ed2d46cfcd24f6529f68819188b8ce6f009dcd4
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2025-04-08 00:09:21 +0800

    refactor: remove: constants.ts

--- lib/android-navigation-bar.ts
+++ lib/android-navigation-bar.ts
@@ -1,9 +1,8 @@
 import * as NavigationBar from "expo-navigation-bar";
 import { Platform } from "react-native";
-import { NAV_THEME } from "~/lib/constants";
 
 export async function setAndroidNavigationBar(theme: "light" | "dark") {
 	if (Platform.OS !== "android") return;
 	await NavigationBar.setButtonStyleAsync(theme === "dark" ? "light" : "dark");
-	await NavigationBar.setBackgroundColorAsync(theme === "dark" ? NAV_THEME.dark.background : NAV_THEME.light.background);
+	await NavigationBar.setBackgroundColorAsync(theme === "dark" ? "hsl(240 10% 3.9%)" : "hsl(0 0% 100%)");
 }
